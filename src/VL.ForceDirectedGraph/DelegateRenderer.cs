907166f599a054af2fbf45feaa51c75646bc7810
Dominik Jancik <magnet.cz@gmail.com>
2020-08-08 16:53:03 +0200

    init

---
+++ src/VL.ForceDirectedGraph/DelegateRenderer.cs
@@ -0,0 +1,35 @@
+﻿using EpForceDirectedGraph.cs;
+
+public class DelegateRenderer : AbstractRenderer
+{
+    public delegate void ConstructorDelegate(IForceDirected iForceDirected);
+    public delegate void ClearDelegate();
+    public delegate void DrawEdgeDelegate(Edge iEdge, AbstractVector iPosition1, AbstractVector iPosition2);
+    public delegate void DrawNodeDelegate(Node iNode, AbstractVector iPosition);
+
+    public ClearDelegate clearDelegate;
+    public DrawEdgeDelegate drawEdgeDelegate;
+    public DrawNodeDelegate drawNodeDelegate;
+
+    public DelegateRenderer(IForceDirected iForceDirected, ClearDelegate clearDelegate, DrawEdgeDelegate drawEdgeDelegate, DrawNodeDelegate drawNodeDelegate) : base(iForceDirected)
+    {
+        this.clearDelegate = clearDelegate;
+        this.drawEdgeDelegate = drawEdgeDelegate;
+        this.drawNodeDelegate = drawNodeDelegate;
+    }
+
+    public override void Clear()
+    {
+        clearDelegate();
+    }
+
+    protected override void drawEdge(Edge iEdge, AbstractVector iPosition1, AbstractVector iPosition2)
+    {
+        drawEdgeDelegate(iEdge, iPosition1, iPosition2);
+    }
+
+    protected override void drawNode(Node iNode, AbstractVector iPosition)
+    {
+        drawNodeDelegate(iNode, iPosition);
+    }
+}
---
+++ src/VL.ForceDirectedGraph/ListRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+using EpForceDirectedGraph.cs;
+
+namespace VL.ForceDirectedGraph
+{
+    public class EdgeBundle
+    {
+        public Edge edge;
+        public AbstractVector from;
+        public AbstractVector to;
+
+        public EdgeBundle(Edge edge, AbstractVector from, AbstractVector to)
+        {
+            this.edge = edge;
+            this.from = from;
+            this.to = to;
+        }
+    }
+
+    public class NodeBundle
+    {
+        public Node node;
+        public AbstractVector position;
+
+        public NodeBundle(Node node, AbstractVector position)
+        {
+            this.node = node;
+            this.position = position;
+        }
+    }
+
+    public class ListRenderer : AbstractRenderer
+    {
+        public List<EdgeBundle> edges;
+        public List<NodeBundle> nodes;
+
+        public ListRenderer(IForceDirected iForceDirected) : base(iForceDirected) {
+            Clear();
+        }
+
+        public override void Clear()
+        {
+            edges = new List<EdgeBundle>();
+            nodes = new List<NodeBundle>();
+        }
+
+        protected override void drawEdge(Edge iEdge, AbstractVector iPosition1, AbstractVector iPosition2)
+        {
+            edges.Add(new EdgeBundle(iEdge, iPosition1, iPosition2));
+        }
+
+        protected override void drawNode(Node iNode, AbstractVector iPosition)
+        {
+            nodes.Add(new NodeBundle(iNode, iPosition));
+        }
+    }
+}
